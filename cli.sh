71e7cda3bcec8c50b9ac8be183f0d7bf55cf5265
Talha Can Havadar <havadartalha@gmail.com>
2026-01-03 22:32:56 +0100

    use classic confinement to not deal with permissions for now

--- cli.sh
+++ cli.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -eux
+set -ex
 export COMPONENT="$SNAP_COMPONENTS/hip"
 # Default OpenAI base path for llama.cpp HTTP server
 export OPENAI_BASE_PATH="/v1"
@@ -10,4 +10,5 @@ export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$COMPONENT/usr/local/lib"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$COMPONENT/usr/local/bin"
 # Add shared libraries added via staged debian packages
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$COMPONENT/usr/lib/$ARCH_TRIPLET"
-exec "$SNAP_COMPONENTS/hip/usr/local/bin/llama-cli"
+
+llama-cli "$@"
