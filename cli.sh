2760ba87a182c84d58145c83f91e5ddd094ad8cd
Talha Can Havadar <havadartalha@gmail.com>
2026-01-03 21:59:07 +0100

    add hip backend build for llama-cpp snap

---
+++ cli.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eux
+export COMPONENT="$SNAP_COMPONENTS/hip"
+# Default OpenAI base path for llama.cpp HTTP server
+export OPENAI_BASE_PATH="/v1"
+# Add llama.cpp binaries
+export PATH="$PATH:$COMPONENT/usr/local/bin"
+# Add llama.cpp-built shared libraries
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$COMPONENT/usr/local/lib"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$COMPONENT/usr/local/bin"
+# Add shared libraries added via staged debian packages
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$COMPONENT/usr/lib/$ARCH_TRIPLET"
+exec "$SNAP_COMPONENTS/hip/usr/local/bin/llama-cli"
